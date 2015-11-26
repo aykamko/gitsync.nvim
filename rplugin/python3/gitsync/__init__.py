d191ee473109c6d3e8ea1080b2767a95f267b832
Aleks Kamko <aykamko@gmail.com>
2015-11-26 15:22:25 -0800

    rudimentary sync validating

--- rplugin/python3/gitsync/__init__.py
+++ rplugin/python3/gitsync/__init__.py
@@ -1,17 +1,12 @@
-import md5
+import neovim
 import os
 import subprocess
-import neovim
-import json
+import time
 # import msgpack
 
+from gitsync import util
 
-def escape(expr):
-    return expr.replace("'", "''")
-
-
-def debug(vim, msg):
-    vim.command('echomsg string(\'' + escape(json.dumps(msg)) + '\')')
+CACHE_DIR = '/tmp/gitsync.nvim'
 
 
 @neovim.plugin
@@ -19,12 +14,75 @@ class GitSyncPlugin:
     def __init__(self, vim):
         self.vim = vim
         self.remote_url = None
+        self.cache_tag = None
+        self.last_poll = 0
+        self.poll_master = None
+        self.poll_seconds = None
+        self.callback = None
 
-    def poll_cache(self):
-        if not os.exists
+    def poll_ref(self, ref, remote):
+        fetch = False
+        cache_entry = '%s/%s_%s' % (CACHE_DIR, self.cache_tag, util.strhash(ref))
+        if not os.path.exists(cache_entry):
+            fetch = True
+        else:
+            lastmodtime = os.stat(cache_entry).st_mtime
+            fetch = (time.time() - lastmodtime) >= self.poll_seconds
 
+        util.touch(cache_entry)
+        if fetch:
+            util.bash('git fetch %s %s' % (remote, ref))
+        return fetch
+
+    def notify_desynced(self, local, remote):
+        for b in self.vim.buffers:
+            old = b.vars.get('gitsync_desynced')
+            diff = util.bash('git diff %s..%s -- %s' % (local, remote, b.name))
+            # b.vars['gitsync_desynced'] = len(diff) > 0
+            new = b.vars['gitsync_desynced'] = True
+            if self.callback and (old is None or old != new):
+                self.vim.command('call %s()' % self.callback)
+
+    @neovim.function('sync_status')
+    def sync_status(self):
+        return self.vim.current.buffer.vars.get('gitsync_desynced', False)
 
     @neovim.command('GitsyncInitPython')
     def init_python(self):
-        self.remote_url = subprocess.check_output('git config --get remote.origin.url')
-        debug(self.vim, self.remote_url)
+        self.poll_master = self.vim.vars.get('gitsync_poll_master', True)
+        self.poll_seconds = 60 * self.vim.vars.get('gitsync_poll_min', 5)
+        self.callback = self.vim.vars.get('gitsync_callback')
+        self.remote_url = util.bash('git config --get remote.origin.url')
+        self.cache_tag = util.strhash(self.remote_url)
+        if not os.path.exists(CACHE_DIR):
+            os.makedirs(CACHE_DIR)
+
+    # @neovim.autocmd('CursorHold,CursorHoldI,CursorMoved,CursorMovedI')
+    @neovim.command('GitsyncSync')
+    def sync(self):
+        if not all((self.remote_url, self.cache_tag)):
+            return
+
+        now = time.time()
+        if (now - self.last_poll) < self.poll_seconds:
+            return
+        self.last_poll = now
+
+        branch = None
+        try:
+            branch = util.bash('git symbolic-ref --short HEAD')
+            ref = util.bash(['git', 'config', '--get', 'branch.%s.merge' % branch])
+            remote = util.bash(['git', 'config', '--get', 'branch.%s.remote' % branch])
+
+            self.poll_ref(ref, remote)
+            self.notify_desynced(branch, ref)
+        except subprocess.CalledProcessError:
+            pass  # TODO
+
+        if self.poll_master and branch and branch != 'master':
+            try:
+                master_ref = util.bash('git config --get branch.master.merge')
+                master_remote = util.bash('git config --get branch.master.remote')
+                self.poll_ref(master_ref, master_remote)
+            except subprocess.CalledProcessError:
+                pass  # TODO
---
+++ rplugin/python3/gitsync/util.py
@@ -0,0 +1,41 @@
+import subprocess
+import os
+import base64
+from remote_pdb import RemotePdb
+
+
+def touch(fname, mode=0o666, dir_fd=None, **kwargs):
+    """
+    Implements the `touch` Unix utility, which set the modification and access time of a file to
+    the current time of day. If the file doesn't exist, it is created with default permissions.
+
+    Source: http://stackoverflow.com/a/1160227
+    """
+    flags = os.O_CREAT | os.O_APPEND
+    with os.fdopen(os.open(fname, flags=flags, mode=mode, dir_fd=dir_fd)) as f:
+        os.utime(f.fileno() if os.utime in os.supports_fd else fname,
+                 dir_fd=None if os.supports_fd else dir_fd, **kwargs)
+
+
+def bash(cmd, strip=True):
+    cmdstr = cmd.split() if (type(cmd) == str) else cmd
+    out = subprocess.check_output(cmdstr, universal_newlines=True)
+    if strip:
+        out = out.strip()
+    return out
+
+
+def strhash(string, encoding='utf8'):
+    if type(string) == bytes:
+        h = base64.urlsafe_b64encode(string)
+    else:
+        h = base64.urlsafe_b64encode(string.encode(encoding))
+    return h.decode(encoding)
+
+
+def pdb(host='127.0.0.1', port=4444):
+    RemotePdb(host, port).set_trace()
+
+
+def debug(vim, msg):
+    vim.command('echomsg "' + msg.replace('"', '""') + '"')
